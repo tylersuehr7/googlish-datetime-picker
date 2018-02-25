0c4bb185fcf13db89cce372db0b3875001efba38
Tyler <tylersuehr@gmail.com>
2018-02-25 01:03:38 -0500

    Added to example for demonstration

--- app/src/main/java/com/tylersuehr/googlishdatetimepickerexample/MainActivity.java
+++ app/src/main/java/com/tylersuehr/googlishdatetimepickerexample/MainActivity.java
@@ -3,11 +3,21 @@ package com.tylersuehr.googlishdatetimepickerexample;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.tylersuehr.googlishdatetimepicker.DateTimePickerLayout;
 
+/**
+ * Copyright © 2017 Tyler Suehr
+ *
+ * Example to demonstrate the use of {@link DateTimePickerLayout}.
+ *
+ * @author Tyler Suehr
+ * @version 1.0
+ */
+public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
